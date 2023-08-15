127b49530cab10fa2f35b4b9cf6f98682ab45cc6
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-15 15:09:05 +0500

    Add Contact Page

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="mt-[60px]">
+            <section id="contact">
+                <div className="px-5 md:py-[3.8rem] py-12 mx-auto">
+                    <div className="flex flex-col text-center w-full">
+                        <h1 className="sm:text-4xl text-3xl font-semibold font-orbitron mb-2 dark:text-white">Contact</h1>
+                    </div>
+                    <div className="md:w-[70%] mx-auto">
+                        <div className="flex flex-wrap -m-2">
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label className="leading-7 text-sm font-semibold font-orbitron dark:text-white">Name</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-slate-400 dark:bg-gray-600 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none dark:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label className="leading-7 text-sm font-semibold font-orbitron dark:text-white">Email</label>
+                                    <input type="email" id="email" name="email" className="w-full bg-slate-400 dark:bg-gray-600 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none dark:text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label className="leading-7 text-sm font-semibold font-orbitron dark:text-white">Message</label>
+                                    <textarea id="message" name="message" className="w-full bg-slate-400 dark:bg-gray-600 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none dark:text-white py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <button className="flex mx-auto text-white border-0 py-2 px-8 focus:outline-none text-lg bg-gradient-to-tr rounded shadow hover:bg-gradient-to-tl from-cyan-600 to-blue-900">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section >
+        </div >
+    )
+};
+
+export default Contact;
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,22 +9,44 @@ import {
 
 const Footer = () => {
     return (
-        <footer className="bg-white dark:bg-gray-900 px-14 border-t border-stone-300 dark:border-stone-700 absolute bottom-0">
-            <div className="px-5 py-8 mx-auto flex items-center sm:flex-row flex-col max-w-7xl">
+        <footer className="bg-white dark:bg-gray-900 px-14 border-t border-stone-300 dark:border-stone-700">
+            <div className="px-5 py-8 mx-auto flex items-center sm:flex-row sm:gap-y-0 gap-y-2 flex-col max-w-7xl">
+                <div className="flex items-center space-x-2">
+                    <h2 className="inline md:hidden text-lg font-orbitron font-bold cursor-pointer tracking-wider">
+                        ACID X
+                    </h2>
+                    <h2 className="hidden md:inline text-lg font-orbitron font-bold cursor-pointer tracking-wider">
+                        ACID X TECH
+                    </h2>
+                </div>
+
                 <p className="sm:inline text-sm dark:text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-600 sm:py-2 sm:mt-0 mt-4 font-semibold">
                     © 2023 <span className="font-orbitron tracking-widest">Ahmëd Shäykh</span>
                 </p>
+
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start space-x-4">
-                    <Link className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://www.instagram.com/AHMXMusic/" target="_blank">
+                    <Link
+                        className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://www.instagram.com/AHMXMusic/"
+                        target="_blank"
+                    >
                         <AiOutlineInstagram className="w-6 h-6" />
                     </Link>
-                    <Link className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://twitter.com/AHMXMusic/" target="_blank">
+                    <Link
+                        className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://twitter.com/AHMXMusic/"
+                        target="_blank"
+                    >
                         <AiOutlineTwitter className="w-6 h-6" />
                     </Link>
-                    <Link className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://github.com/AhmedShaykh/" target="_blank">
+                    <Link
+                        className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://github.com/AhmedShaykh/"
+                        target="_blank"
+                    >
                         <AiOutlineGithub className="w-6 h-6" />
                     </Link>
-                    <Link className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://www.linkedin.com/in/ahmed-saleem-shaikh-73955b251/" target="_blank">
+                    <Link
+                        className="dark:text-white cursor-pointer hover:text-slate-500 dark:hover:text-blue-300" href="https://www.linkedin.com/in/ahmed-saleem-shaikh-73955b251/"
+                        target="_blank"
+                    >
                         <AiOutlineLinkedin className="w-6 h-6" />
                     </Link>
                 </span>
