f04973897664e61631c86a0c27f4a3e9cb1541da
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-17 23:07:32 +0500

    Banner Data Fetch

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,27 +1,55 @@
 import React from "react";
-import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-const Home = () => {
+async function getBanner() {
+
+    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=banner`, { cache: 'no-cache' });
+
+    if (!res.ok) {
+        throw new Error('Failed to Fetch Data');
+    }
+
+    return res.json();
+
+};
+
+
+const Home = async () => {
+
+    const banner = await getBanner();
+
     return (
         <div className="mt-24">
             <section id="home" className="bg-[#e5e7eb] dark:bg-[#04040e] dark:text-white body-font">
-                <div className="mx-auto flex py-16 md:py-24 md:flex-row flex-col items-center max-w-7xl">
-                    <div className="flex justify-center items-center px-12">
-                        <Image
-                            className="object-cover object-center rounded-md"
-                            src=""
-                            alt=""
-                            width={300}
-                            height={300}
-                        />
-                    </div>
-                    <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center max-w-3xl px-8 lg:px-12">
-                        <h1 className="text-4xl font-orbitron font-bold mt-6 md:mt-0 md:text-5xl">ACID X TECH</h1>
-                        <p className="text-lg font-orbitron font-semibold mt-4 mb-6 md:text-[1.4rem] md:py-4 py-2">
-                            The Future Is Here!
-                        </p>
-                    </div>
-                </div>
+
+                {banner.items.map((item: any) => {
+
+                    const bannerImg = banner.includes.Asset.find((img: any) => img.sys.id === item.fields.heroImage.sys.id);
+
+                    return (
+                        <div className="mx-auto flex py-16 md:py-24 md:flex-row flex-col items-center max-w-7xl">
+                            <div className="flex justify-center items-center px-12">
+                                <img
+                                    className="object-cover object-center rounded-md"
+                                    src={bannerImg.fields.file.url}
+                                    alt="Hero"
+                                    width={300}
+                                    height={300}
+                                />
+                            </div>
+
+                            <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center max-w-3xl px-8 lg:px-12">
+                                <h1 className="text-4xl font-orbitron font-bold mt-6 md:mt-0 md:text-5xl">
+                                    {item.fields.title}
+                                </h1>
+
+                                <p className="text-lg font-orbitron font-semibold mt-4 mb-6 md:text-[1.4rem] md:py-4 py-2">
+                                    {documentToReactComponents(item.fields.description)}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
             </section>
         </div>
     )
