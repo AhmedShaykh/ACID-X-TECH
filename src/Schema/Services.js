d6fbfcfff75d41e14b010c606e66ea678bc1d72a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-18 16:32:00 +0500

    Add Services Schema

--- src/Schema/Services.js
+++ src/Schema/Services.js
@@ -1,1 +1,175 @@
-const services = {};
+const services = {
+    "sys": {
+        "type": "Array"
+    },
+    "total": 3,
+    "skip": 0,
+    "limit": 100,
+    "items": [
+        {
+            "metadata": {
+                "tags": []
+            },
+            "sys": {
+                "space": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "Space",
+                        "id": "gyr0iwh7sl8k"
+                    }
+                },
+                "id": "35RJWdlR72bj3Rm25Cxmqd",
+                "type": "Entry",
+                "createdAt": "2023-08-18T10:58:15.107Z",
+                "updatedAt": "2023-08-18T11:23:12.642Z",
+                "environment": {
+                    "sys": {
+                        "id": "master",
+                        "type": "Link",
+                        "linkType": "Environment"
+                    }
+                },
+                "revision": 5,
+                "contentType": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "ContentType",
+                        "id": "services"
+                    }
+                },
+                "locale": "en-US"
+            },
+            "fields": {
+                "title": "UI/UX Designing & 3D (AR/VR) Model",
+                "description": {
+                    "data": {},
+                    "content": [
+                        {
+                            "data": {},
+                            "content": [
+                                {
+                                    "data": {},
+                                    "marks": [],
+                                    "value": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                    "nodeType": "text"
+                                }
+                            ],
+                            "nodeType": "paragraph"
+                        }
+                    ],
+                    "nodeType": "document"
+                }
+            }
+        },
+        {
+            "metadata": {
+                "tags": []
+            },
+            "sys": {
+                "space": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "Space",
+                        "id": "gyr0iwh7sl8k"
+                    }
+                },
+                "id": "27crXbZ4tbuxI2wazJVkHW",
+                "type": "Entry",
+                "createdAt": "2023-08-18T10:56:49.388Z",
+                "updatedAt": "2023-08-18T11:22:18.802Z",
+                "environment": {
+                    "sys": {
+                        "id": "master",
+                        "type": "Link",
+                        "linkType": "Environment"
+                    }
+                },
+                "revision": 5,
+                "contentType": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "ContentType",
+                        "id": "services"
+                    }
+                },
+                "locale": "en-US"
+            },
+            "fields": {
+                "title": "Full Stack Blockchain , NFT & Smart Contract Development",
+                "description": {
+                    "data": {},
+                    "content": [
+                        {
+                            "data": {},
+                            "content": [
+                                {
+                                    "data": {},
+                                    "marks": [],
+                                    "value": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                    "nodeType": "text"
+                                }
+                            ],
+                            "nodeType": "paragraph"
+                        }
+                    ],
+                    "nodeType": "document"
+                }
+            }
+        },
+        {
+            "metadata": {
+                "tags": []
+            },
+            "sys": {
+                "space": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "Space",
+                        "id": "gyr0iwh7sl8k"
+                    }
+                },
+                "id": "6v15MNlo6CjPmpfVzhfnFK",
+                "type": "Entry",
+                "createdAt": "2023-08-18T10:53:41.634Z",
+                "updatedAt": "2023-08-18T11:21:17.761Z",
+                "environment": {
+                    "sys": {
+                        "id": "master",
+                        "type": "Link",
+                        "linkType": "Environment"
+                    }
+                },
+                "revision": 4,
+                "contentType": {
+                    "sys": {
+                        "type": "Link",
+                        "linkType": "ContentType",
+                        "id": "services"
+                    }
+                },
+                "locale": "en-US"
+            },
+            "fields": {
+                "title": "Full Stack Development & AI Integration Web/Apps",
+                "description": {
+                    "nodeType": "document",
+                    "data": {},
+                    "content": [
+                        {
+                            "nodeType": "paragraph",
+                            "data": {},
+                            "content": [
+                                {
+                                    "nodeType": "text",
+                                    "value": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                    "marks": [],
+                                    "data": {}
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+};
