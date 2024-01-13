e41d524e80baadedf706e44616f1a80dd4a61118
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-01-13 19:08:56 +0500

    Create Schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,10 @@
 import { type SchemaTypeDefinition } from "sanity";
+import blockContent from "./schemas/blockContent";
+import category from "./schemas/category";
+import product from "./schemas/product";
+import banner from "./schemas/banner";
+import order from "./schemas/order";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [banner, blockContent, category, order, product],
 };
