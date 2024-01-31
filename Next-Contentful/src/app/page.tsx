a0997ccb377bb65920ef795b90f4fa28c97fd286
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-01 02:14:30 +0500

    Final Commit

---
+++ Next-Contentful/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "@/Components/Home";
+import Services from "@/Components/Services";
+import Contact from "@/Components/Contact";
+
+const page = () => {
+    return (
+        <>
+            <Home />
+            <Services />
+            <Contact />
+        </>
+    )
+};
+
+export default page;
